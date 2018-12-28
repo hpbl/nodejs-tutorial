3beb177d6011cb529bb61bfe7d2bb7000543cd72
Hilton Pintor <hiltonpintor@gmail.com>
2018-12-28 12:28:07 -0300

    Pipes and Pipe Chaining. (Readable,Writable and Transform Streams)

--- using-streams.js
+++ using-streams.js
@@ -2,6 +2,16 @@ const fs = require('fs');
 
 const readStream = fs.createReadStream('./example.txt', 'utf8');
 const writeStream = fs.createWriteStream('./example2.txt')
-readStream.on('data', chunk => {
+/*readStream.on('data', chunk => {
   writeStream.write(chunk);
-})
+})*/
+
+
+// Pipes and Pipe Chaining. (Readable,Writable and Transform Streams) //
+/* readStream.pipe(writeStream);*/
+
+// Compressing file before writing
+const zlib = require('zlib');
+const gzip = zlib.createGzip();
+const writeGzipStream = fs.createWriteStream('./example2.txt.gz')
+readStream.pipe(gzip).pipe(writeGzipStream);
