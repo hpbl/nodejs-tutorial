bc19e21b764c6fc89204c24e06d128703b6db5b4
Hilton Pintor <hiltonpintor@gmail.com>
2018-12-28 12:38:24 -0300

    Creating a Http Server using the Http Module

---
+++ http.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.write('Hello world, from nodejs');
+  } else {
+    res.write('using some other domain');
+  }
+
+  res.end();
+})
+
+server.listen('3000');
