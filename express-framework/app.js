bba1018298bde0ee7b958f9c1d7a0a36c1153705
Hilton Pintor <hiltonpintor@gmail.com>
2018-12-30 19:25:48 -0300

    Working with Express Http Get Request, Route Params and Query Strings

--- express-framework/app.js
+++ express-framework/app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
 
+app.listen(3000);
+
 app.get('/', (req, res) => {
   res.send('Hello world!');
 });
 
-app.listen(3000);
+app.get('/example', (req, res) => {
+  res.send('Hello example!');
+});
+
+app.get('/example/:name/:age', (req, res) => {
+  console.log(req.params); // route parameters (data we need)
+  console.log(req.query); // query parameters (optional data)
+  const { name, age } = req.params;
+  res.send(name + " : " + age);
+})
