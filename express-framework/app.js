81f369fdd5c901197ffea79bfbd50d1abb52523a
Hilton Pintor <hiltonpintor@gmail.com>
2019-01-02 14:56:54 -0300

    Http Post Request with Express and Body Parser Module

--- express-framework/app.js
+++ express-framework/app.js
@@ -24,3 +24,13 @@ app.use('/public', express.static(path.join(__dirname, 'static')));
 app.get('/file', (req, res) => {
   res.sendFile(path.join(__dirname, 'static', 'index.html'));
 });
+
+
+// Post Request //
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.post('/', (req, res) => {
+  console.log(req.body);
+  // database work here
+  res.send('successfully posted data');
+});
